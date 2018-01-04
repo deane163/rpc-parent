b8b7f1156ddf56bf13d452e0c731a0b2a2c48557
deane163 <deane163@126.com>
2018-01-04 17:01:41 +0800

    【功能】添加annotation

---
+++ rpc-server/src/main/java/com/xiaoshu/rpc/server/RpcService.java
@@ -0,0 +1,48 @@
+package com.xiaoshu.rpc.server;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description :  RPC 服务注解（标注在服务实现类上）
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2018年1月4日下午4:59:37
+ * 
+ * Copyright (C)2013-2018 小树盛凯科技 All rights reserved.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface RpcService {
+	// 服务接口类
+    Class<?> value();
+    
+    // 版本号
+    String version() default "";
+}
