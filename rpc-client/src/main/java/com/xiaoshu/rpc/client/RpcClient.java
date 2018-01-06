3bb6746972d43f5a5a17c60b8a40aa23cba0fa3c
deane163 <deane163@126.com>
2018-01-06 17:27:09 +0800

    【功能】添加服务类

---
+++ rpc-client/src/main/java/com/xiaoshu/rpc/client/RpcClient.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package com.xiaoshu.rpc.client;
+
+
+import com.xiaoshu.rpc.common.bean.RpcRequest;
+import com.xiaoshu.rpc.common.bean.RpcResponse;
+import com.xiaoshu.rpc.common.codec.RpcDecoder;
+import com.xiaoshu.rpc.common.codec.RpcEncoder;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description : 
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2018年1月6日 下午5:00:51
+ * 
+ * Copyright (C)2013-2018 小树盛凯科技 All rights reserved.
+ */
+public class RpcClient extends SimpleChannelInboundHandler<RpcResponse> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpcClient.class);
+    
+    private final String host;
+    private final int port;
+
+    private RpcResponse response;
+
+    public RpcClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, RpcResponse msg)
+            throws Exception {
+        this.response = response;
+    }
+    
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        LOGGER.error("api caught exception", cause);
+        ctx.close();
+    }
+    
+    public RpcResponse send(RpcRequest request) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            // 创建并初始化 Netty 客户端 Bootstrap 对象
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group);
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel channel) throws Exception {
+                    ChannelPipeline pipeline = channel.pipeline();
+                    pipeline.addLast(new RpcEncoder(RpcRequest.class)); // 编码 RPC 请求
+                    pipeline.addLast(new RpcDecoder(RpcResponse.class)); // 解码 RPC 响应
+                    pipeline.addLast(RpcClient.this); // 处理 RPC 响应
+                }
+            });
+            bootstrap.option(ChannelOption.TCP_NODELAY, true);
+            // 连接 RPC 服务器
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            // 写入 RPC 请求数据并关闭连接
+            Channel channel = future.channel();
+            channel.writeAndFlush(request).sync();
+            channel.closeFuture().sync();
+            // 返回 RPC 响应对象
+            return response;
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ rpc-client/src/main/java/com/xiaoshu/rpc/client/RpcProxy.java
@@ -0,0 +1,118 @@
+/**
+ * 
+ */
+package com.xiaoshu.rpc.client;
+
+import com.xiaoshu.rpc.common.bean.RpcRequest;
+import com.xiaoshu.rpc.common.bean.RpcResponse;
+import com.xiaoshu.rpc.common.util.StringUtil;
+import com.xiaoshu.rpc.registry.ServiceDiscovery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.UUID;
+
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description : 
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2018年1月6日 下午5:05:42
+ * 
+ * Copyright (C)2013-2018 小树盛凯科技 All rights reserved.
+ */
+public class RpcProxy {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpcProxy.class);
+
+    private String serviceAddress;
+
+    private ServiceDiscovery serviceDiscovery;
+
+    public RpcProxy(String serviceAddress) {
+        this.serviceAddress = serviceAddress;
+    }
+
+    public RpcProxy(ServiceDiscovery serviceDiscovery) {
+        this.serviceDiscovery = serviceDiscovery;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public <T> T create(final Class<?> interfaceClass) {
+        return create(interfaceClass, "");
+    }
+    
+    @SuppressWarnings("unchecked")
+    public <T> T create(final Class<?> interfaceClass, final String serviceVersion) {
+        // 创建动态代理对象
+        return (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(),new Class<?>[]{interfaceClass},
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        // 创建 RPC 请求对象并设置请求属性
+                        RpcRequest request = new RpcRequest();
+                        request.setRequestId(UUID.randomUUID().toString());
+                        request.setInterfaceName(method.getDeclaringClass().getName());
+                        request.setServiceVersion(serviceVersion);
+                        request.setMethodName(method.getName());
+                        request.setParameterTypes(method.getParameterTypes());
+                        request.setParameters(args);
+                        // 获取 RPC 服务地址
+                        if (serviceDiscovery != null) {
+                            String serviceName = interfaceClass.getName();
+                            if (StringUtil.isNotEmpty(serviceVersion)) {
+                                serviceName += "-" + serviceVersion;
+                            }
+                            serviceAddress = serviceDiscovery.discovery(serviceName);
+                            LOGGER.debug("discover service: {} => {}", serviceName, serviceAddress);
+                        }
+                        if (StringUtil.isEmpty(serviceAddress)) {
+                            throw new RuntimeException("server address is empty");
+                        }
+                        // 从 RPC 服务地址中解析主机名与端口号
+                        String[] array = StringUtil.split(serviceAddress, ":");
+                        String host = array[0];
+                        int port = Integer.parseInt(array[1]);
+                        // 创建 RPC 客户端对象并发送 RPC 请求
+                        RpcClient client = new RpcClient(host, port);
+                        long time = System.currentTimeMillis();
+                        RpcResponse response = client.send(request);
+                        LOGGER.debug("time: {}ms", System.currentTimeMillis() - time);
+                        if (response == null) {
+                            throw new RuntimeException("response is null");
+                        }
+                        // 返回 RPC 响应结果
+                        if (response.hasException()) {
+                            throw response.getException();
+                        } else {
+                            return response.getResult();
+                        }
+                    }
+                }
+        );
+    }
+
+}
