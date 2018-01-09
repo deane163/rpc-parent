01a63b1bedd602be4566f8856587ba8c2bd9aaff
deane163 <deane163@126.com>
2018-01-09 17:08:11 +0800

    【功能】添加注解信息

--- rpc-registry/src/main/java/com/xiaoshu/RgistryPackage.java
+++ rpc-registry/src/main/java/com/xiaoshu/RgistryPackage.java
@@ -3,6 +3,7 @@ package com.xiaoshu;
 /**
  *
  * @Description : 声明 服务注册和服务发现的接口声明
+ * 实现类可以是多种，可以是Zookeeper、Redis、或其它存储服务
  * ---------------------------------
  * @Author : deane.administrator
  * @Date : Create in 2018年1月4日下午5:13:05
