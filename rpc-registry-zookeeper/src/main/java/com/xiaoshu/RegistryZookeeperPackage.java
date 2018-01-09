ac86b1a135c15bd1eff3add53011979eb9e05356
deane163 <deane163@126.com>
2018-01-09 17:06:42 +0800

    【功能】添加注解信息

--- rpc-registry-zookeeper/src/main/java/com/xiaoshu/RegistryZookeeperPackage.java
+++ rpc-registry-zookeeper/src/main/java/com/xiaoshu/RegistryZookeeperPackage.java
@@ -4,6 +4,7 @@ package com.xiaoshu;
 
  *
  * @Description : 将服务信息 注册到Zookeeper服务器上面
+ * 注册服务的实现类，同样可以注册到Redis或文件存储中
  * ---------------------------------
  * @Author : deane.administrator
  * @Date : Create in 2018年1月4日下午5:20:29
